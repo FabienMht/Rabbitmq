136ff4ad9635228810e21614b7ce0e1708dad1df
FabienMht <32060128+FabienMht@users.noreply.github.com>
2019-12-06 13:40:31 +0100

    Code commanditaire

---
+++ commanditaire.py
@@ -0,0 +1,94 @@
+try:
+    import requests,json,os,pika,sys
+    from git import Repo,Actor
+except ModuleNotFoundError as error:
+    print(error.__class__.__name__ + ": " + error.name)
+    exit(1)
+
+def help():
+    print("Utilisation : python3 commanditaire.py x (ou x est la taille de l echiquier)")
+    exit(1)
+
+def create_file(address,file):
+    param = {"data" : json.dumps(file)}
+    r = requests.post("http://"+address+":5000/rabbit/create",data=param)
+    print(r.text)
+
+def purge_file(address,file):
+    param = {"data" : json.dumps(file)}
+    r = requests.post("http://"+address+":5000/rabbit/purge",data=param)
+    print(r.text)
+
+def send_msg(address,tache,file):
+    param = {"data" : json.dumps(tache)}
+    r = requests.post("http://"+address+":5000/rabbit/sendmsg/"+file,data=param)
+    print(r.text)
+    print(tache)
+
+def on_msg(channel, method, properties, body):
+    global resultat
+    print (body)
+    resultat_json=json.loads(body.decode())
+    resultat_temp=resultat_json["Resultat"]
+    resultat_id_tache=resultat_json["ID_tache"]
+    nb_taches.remove(int(resultat_id_tache))
+    resultat=resultat+int(resultat_temp)
+    
+    if len(nb_taches)==0:
+        channel.stop_consuming()
+    
+# Variables
+author = Actor("Fabien M", "mauhouratfabien@gmail.com")
+git_url_repo="git@github.com:FabienMht/Rabbitmq.git"
+repo_workdir="./Project/Rabbitmq"
+project_files="./Project/Files"
+commit_msg="RT704"
+file_tache="ToDo"
+file_resultat="Done"
+flask_address="localhost"
+rabbitmq_address=sys.argv[1]
+nb_taches=[]
+resultat=0
+
+# Test du paramètre qui définie la taille de l'échiquier
+try:
+    chess_size=int(sys.argv[2])
+except IndexError:
+    help()
+
+# Test de connexion à la file Rabbitmq
+try:
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_address))
+    connection.close()
+except pika.exceptions.AMQPConnectionError:
+    print("Erreur : connexion impossible a la file RabbitMq "+rabbitmq_address)
+    exit(1)
+
+# Création des files
+for list in file_tache,file_resultat:
+    file={}
+    file["file"] = list
+    purge_file(flask_address,file)
+    create_file(flask_address,file)
+
+# Génération des taches
+for i in range(chess_size):
+    tache={}
+    tache["ID_projet"] = 1
+    tache["ID_tache"] = i
+    tache["URL_git"] = git_url_repo
+    tache["CMD"] = "python3 /n_dame.py ["+str(i)+",-1,-1,-1,-1,-1,-1,-1]"
+    nb_taches.append(int(tache["ID_tache"]))
+    send_msg(flask_address,tache,file_tache)
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_address))
+channel = connection.channel()
+print("En attente de message")
+channel.basic_consume(file_resultat,on_msg,auto_ack=True)
+try:
+    channel.start_consuming()
+except KeyboardInterrupt:
+    channel.stop_consuming()
+connection.close()
+
+print("Nombre de solutions :",resultat)
---
+++ rabbitmq.py
@@ -0,0 +1,87 @@
+try:
+    import pika,json,sys
+    from flask import Flask, request
+except ModuleNotFoundError as error:
+    print(error.__class__.__name__ + ": " + error.name)
+    exit(1)
+
+# Variables
+app = Flask(__name__)
+rabbitmq_address=sys.argv[1]
+
+def create(address,file):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=address))
+    channel = connection.channel()
+    channel.queue_declare(queue=file)
+    connection.close()
+    return True
+
+def purge(address,file):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=address))
+    channel = connection.channel()
+    if channel.queue_declare(queue=file,passive=True):
+        channel.queue_purge(queue=file)
+    connection.close()
+    return True
+    
+def write(address,msg,file):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=address))
+    channel = connection.channel()
+    channel.basic_publish(exchange='', routing_key=file, body=json.dumps(msg))
+    connection.close()
+    return True
+
+def read(address,file):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=address))
+    channel = connection.channel()
+    method_frame, header_frame, body = channel.basic_get(file)
+    if method_frame:
+        print(method_frame, header_frame, body)
+        channel.basic_ack(method_frame.delivery_tag)
+        return body
+    else:
+        return "False"
+
+# Test de connexion à la file Rabbitmq
+try:
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_address))
+    connection.close()
+except pika.exceptions.AMQPConnectionError:
+    print("Erreur : connexion impossible a la file RabbitMq "+rabbitmq_address)
+    exit(1)
+
+# Définition des routes
+@app.route("/rabbit/create",methods=['POST'])
+def f1():
+    file = json.loads(request.form["data"])
+    
+    if create(rabbitmq_address,file["file"]):
+        return "Creation de la file termine"
+    else:
+        return "Erreur creation de file"
+
+@app.route("/rabbit/sendmsg/<file_name>",methods=['POST'])
+def f2(file_name=None):
+    msg = json.loads(request.form["data"])
+    
+    if write(rabbitmq_address,msg,file_name):
+        return "Message parti"
+    else:
+        return "Erreur envoi de message"
+
+@app.route("/rabbit/getmsg/<file_name>",methods=['GET'])
+def f4(file_name=None):
+    return read(rabbitmq_address,file_name)
+
+@app.route("/rabbit/purge",methods=['POST'])
+def f3():
+    file = json.loads(request.form["data"])
+    
+    if purge(rabbitmq_address,file["file"]):
+        return "Purge de la file termine"
+    else:
+        return "Purge creation de file"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000,debug=True)
+
