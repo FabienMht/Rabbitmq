88246046f3e96daad6451b3c1e79eca0e0d025f9
Fabien M <mauhouratfabien@gmail.com>
2019-12-14 11:53:49 +0100

    Ajout docker-compose

---
+++ Docker/Commanditaire/commanditaire.py
@@ -0,0 +1,102 @@
+try:
+    import requests,json,os,pika,sys
+except ModuleNotFoundError as error:
+    print(error.__class__.__name__ + ": " + error.name)
+    exit(1)
+
+def help():
+    print("Utilisation : python3 commanditaire.py x (ou x est la taille de l echiquier)")
+    exit(1)
+
+def create_file(address,file):
+    param = {"data" : json.dumps(file)}
+    r = requests.post("http://"+address+":5000/rabbit/create",data=param)
+    print(r.text)
+
+def purge_file(address,file):
+    param = {"data" : json.dumps(file)}
+    r = requests.post("http://"+address+":5000/rabbit/purge",data=param)
+    print(r.text)
+
+def send_msg(address,tache,file):
+    param = {"data" : json.dumps(tache)}
+    r = requests.post("http://"+address+":5000/rabbit/sendmsg/"+file,data=param)
+    print(r.text)
+    print(tache)
+
+def on_msg(channel, method, properties, body):
+    global resultat
+    print (body)
+    resultat_json=json.loads(body.decode())
+    resultat_temp=resultat_json["Resultat"]
+    resultat_id_tache=resultat_json["ID_tache"]
+    nb_taches.remove(int(resultat_id_tache))
+    resultat=resultat+int(resultat_temp)
+    
+    if len(nb_taches)==0:
+        channel.stop_consuming()
+    
+# Variables
+git_url_repo="https://github.com/FabienMht/N_dames.git"
+commit_msg="RT704"
+file_tache="ToDo"
+file_resultat="Done"
+flask_address="flask"
+rabbitmq_address="rabbitmq"
+nb_taches=[]
+resultat=0
+
+os.system("""curl --silent rabbitmq:5672 > /dev/null
+while [[ "$?" = "7" ]]; do
+  sleep 1
+  curl --silent rabbitmq:5672 > /dev/null
+done""")
+
+os.system("""curl --silent flask:5000 > /dev/null
+while [[ "$?" = "7" ]]; do
+  sleep 1
+  curl --silent flask:5000 > /dev/null
+done""")
+
+# Test du paramètre qui définie la taille de l'échiquier
+chess_size=int(os.environ['TAILLE'])
+
+# Test de connexion à la file Rabbitmq
+try:
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_address))
+    connection.close()
+except pika.exceptions.AMQPConnectionError:
+    print("Erreur : connexion impossible a la file RabbitMq "+rabbitmq_address)
+    exit(1)
+
+# Création des files
+for list in file_tache,file_resultat:
+    file={}
+    file["file"] = list
+    purge_file(flask_address,file)
+    create_file(flask_address,file)
+
+sol=[-1]*chess_size
+
+# Génération des taches
+for i in range(chess_size):
+    sol[0]=i
+    tache={}
+    tache["ID_projet"] = 1
+    tache["ID_tache"] = i
+    tache["URL_git"] = git_url_repo
+    tache["CMD"] = "python3 /n_dame.py "+str(sol).replace(" ","")
+    nb_taches.append(int(tache["ID_tache"]))
+    send_msg(flask_address,tache,file_tache)
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_address))
+channel = connection.channel()
+print("En attente de message")
+channel.basic_consume(file_resultat,on_msg,auto_ack=True)
+try:
+    channel.start_consuming()
+except KeyboardInterrupt:
+    channel.stop_consuming()
+connection.close()
+
+print("Nombre de solutions :",resultat)
---
+++ Docker/Worker/worker.py
@@ -0,0 +1,83 @@
+try:
+    import requests,json,docker,time,sys,os
+    from git import Repo,Actor
+except ModuleNotFoundError as error:
+    print(error.__class__.__name__ + ": " + error.name)
+    exit(1)
+
+def get_tache(address,file):
+    try:
+        r = requests.get("http://"+address+":5000/rabbit/getmsg/"+file)
+    except Exception as e:
+        return "False"
+    return r.text
+
+def send_msg(address,resultat,file):
+    param = {"data" : json.dumps(resultat)}
+    r = requests.post("http://"+address+":5000/rabbit/sendmsg/"+file,data=param)
+    print(r.text)
+
+def clone_repo(git_url,repo_dir):
+    try:
+        repo = Repo(repo_dir)
+        for url in repo.remotes.origin.urls:
+            if url==git_url:
+                print("Dépot "+(git_url.split('/')[-1]).replace('.git','')+" déjà cloné dans "+repo.working_dir)
+    except Exception as e:
+        repo = Repo.clone_from(git_url, repo_dir)
+        print("Dépot "+(git_url.split('/')[-1]).replace('.git','')+" cloné dans "+repo.working_dir)
+    return repo
+    
+def pull_repo(repo):
+    repo.remotes.origin.pull()
+    print("Pull effectué")
+    
+def exec_container(client,image_name,command,detach):
+    cont=client.containers.run(image=image_name, command=command, detach=detach, auto_remove=True)
+    print("Container lancé")
+    return cont
+    
+def build_container(client,path,tag):
+    client.images.build(path=path,tag=tag)
+    print("Build effectué")
+
+# Variables
+client = docker.from_env()
+image_name = "rt704"
+repo_workdir = "/N_Dames"
+file_tache="ToDo"
+file_resultat="Done"
+flask_address="flask"
+
+# Check flask connexion
+os.system("""curl --silent flask:5000 > /dev/null
+while [[ "$?" = "7" ]]; do
+  sleep 1
+  curl --silent flask:5000 > /dev/null
+done""")
+
+#time.sleep(5)
+
+# Code
+try:
+    while True:
+        tache = get_tache(flask_address,file_tache)
+        if tache!="False":
+            print(tache)
+            tache_json = json.loads(tache)
+            repo=clone_repo(tache_json["URL_git"],repo_workdir)
+            pull_repo(repo)
+            build_container(client,repo_workdir,image_name)
+            resultat=exec_container(client,image_name,tache_json["CMD"],False)
+            resultat_json={}
+            resultat_json["ID_projet"] = int(tache_json["ID_projet"])
+            resultat_json["ID_tache"] = int(tache_json["ID_tache"])
+            resultat_json["Resultat"] = resultat.decode().strip('\n')
+            print(resultat_json)
+            send_msg(flask_address,resultat_json,file_resultat)
+        else:
+            print("Aucune tache !")
+            exit(0)
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Fin")
